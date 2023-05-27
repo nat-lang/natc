4cf4a7a25927f56496af761289211cbe57baab9d
Alex Shilen <alexander.shilen@flocksafety.com>
2023-05-27 16:22:27 -0400

    types

--- src/main.c
+++ src/main.c
@@ -67,7 +67,7 @@ int main(int argc, const char* argv[]) {
   } else if (argc == 2) {
     runFile(argv[1]);
   } else {
-    fprintf(stderr, "Usage: clox [path]\n");
+    fprintf(stderr, "Usage: nat [path]\n");
     exit(64);
   }
 
