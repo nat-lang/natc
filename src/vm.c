260729e98c659abf2ab533bfdd2f8db744f0c234
Alexander Shilen <aps2163@columbia.edu>
2024-01-01 15:52:15 -0500

    gc

--- src/vm.c
+++ src/vm.c
@@ -56,6 +56,14 @@ static void defineNative(const char* name, int arity, NativeFn function) {
 void initVM() {
   resetStack();
   vm.objects = NULL;
+
+  vm.bytesAllocated = 0;
+  vm.nextGC = 1024 * 1024;
+
+  vm.grayCount = 0;
+  vm.grayCapacity = 0;
+  vm.grayStack = NULL;
+
   initTable(&vm.globals);
   initTable(&vm.strings);
 
