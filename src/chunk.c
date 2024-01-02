ea6ff611d5bc90b8c5008b7557cbeeebad00fb3e
Alexander Shilen <aps2163@columbia.edu>
2024-01-01 20:22:09 -0500

    more gc

--- src/chunk.c
+++ src/chunk.c
@@ -1,7 +1,9 @@
+#include "chunk.h"
+
 #include <stdlib.h>
 
-#include "chunk.h"
 #include "memory.h"
+#include "vm.h"
 
 void initChunk(Chunk* chunk) {
   chunk->count = 0;
@@ -22,10 +24,9 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
   if (chunk->capacity < chunk->count + 1) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
-    chunk->code = GROW_ARRAY(uint8_t, chunk->code,
-        oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(int, chunk->lines,
-        oldCapacity, chunk->capacity);
+    chunk->code =
+        GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
@@ -34,6 +35,8 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
 }
 
 int addConstant(Chunk* chunk, Value value) {
+  push(value);
   writeValueArray(&chunk->constants, value);
+  pop();
   return chunk->constants.count - 1;
 }
