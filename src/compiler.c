fa94b104430d83d6f3ab2c782355d393802807da
Alexander Shilen <aps2163@columbia.edu>
2024-01-08 00:37:50 -0500

    @initialization

--- src/compiler.c
+++ src/compiler.c
@@ -53,7 +53,7 @@ typedef struct {
   bool isLocal;
 } Upvalue;
 
-typedef enum { TYPE_FUNCTION, TYPE_SCRIPT } FunctionType;
+typedef enum { TYPE_FUNCTION, TYPE_METHOD, TYPE_SCRIPT } FunctionType;
 
 typedef struct Compiler {
   struct Compiler* enclosing;
@@ -66,9 +66,14 @@ typedef struct Compiler {
   int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler {
+  struct ClassCompiler* enclosing;
+} ClassCompiler;
+
 Parser parser;
 static Parser checkpoint;
 Compiler* current = NULL;
+ClassCompiler* currentClass = NULL;
 
 static Chunk* currentChunk() { return &current->function->chunk; }
 
@@ -213,9 +218,13 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
 
   Local* local = &current->locals[current->localCount++];
   local->depth = 0;
-  local->isCaptured = false;
-  local->name.start = "";
-  local->name.length = 0;
+  if (type == TYPE_METHOD) {
+    local->name.start = "this";
+    local->name.length = 4;
+  } else {
+    local->name.start = "";
+    local->name.length = 0;
+  }
 }
 
 static ObjFunction* endCompiler() {
@@ -503,6 +512,14 @@ static void variable(bool canAssign) {
   namedVariable(parser.previous, canAssign);
 }
 
+static void this_(bool canAssign) {
+  if (currentClass == NULL) {
+    error("Can't use 'this' outside of a class.");
+    return;
+  }
+  variable(false);
+}
+
 static void unary(bool canAssign) {
   TokenType operatorType = parser.previous.type;
 
@@ -692,7 +709,7 @@ static void function(FunctionType type) {
 static void method() {
   consume(TOKEN_IDENTIFIER, "Expect method name.");
   uint8_t constant = identifierConstant(&parser.previous);
-  FunctionType type = TYPE_FUNCTION;
+  FunctionType type = TYPE_METHOD;
   function(type);
   emitBytes(OP_METHOD, constant);
 }
@@ -706,6 +723,11 @@ static void classDeclaration() {
 
   emitBytes(OP_CLASS, nameConstant);
   defineVariable(nameConstant);
+
+  ClassCompiler classCompiler;
+  classCompiler.enclosing = currentClass;
+  currentClass = &classCompiler;
+
   namedVariable(className, false);
 
   consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
@@ -713,7 +735,11 @@ static void classDeclaration() {
     method();
   }
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
-  emitByte(OP_POP);  // pop the classname
+
+  // pop the classname
+  emitByte(OP_POP);
+
+  currentClass = currentClass->enclosing;
 }
 
 static void letDeclaration() {
