d7f9f4474cae2513a97541ee9068b7d96108dfa7
Alex Shilen <ashilen1@jh.edu>
2024-03-06 23:14:20 -0500

    link libm

--- src/memory.c
+++ src/memory.c
@@ -22,7 +22,7 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 #endif
 
     if (vm.bytesAllocated > vm.nextGC) {
-      // collectGarbage();
+      collectGarbage();
     }
   }
 
