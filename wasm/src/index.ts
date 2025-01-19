95df5fd531d71760a40c9157d95411f694d1052b
Alex Shilen <ashilen1@jh.edu>
2025-01-19 13:46:18 -0500

    feat: bundle wasm package with client

--- wasm/src/index.ts
+++ wasm/src/index.ts
@@ -1,3 +1,162 @@
-import Module, { NatModule } from "../lib/nat";
+import { v4 } from 'uuid';
+import initialize, { NatModule } from "../lib/nat";
 
-export { Module, type NatModule }
+export type CompilationNode = {
+  children: CompilationNode[];
+  name: string;
+  type: string;
+  tex: string;
+  html?: string;
+}
+
+export type Compilation = {
+  success: boolean;
+  data: CompilationNode[];
+}
+
+export type CoreFile = {
+  path: string;
+  content: string;
+  type: "tree" | "blob";
+};
+
+type OutputHandler = (stdout: string) => void;
+type OutputHandlerMap = { [key: string]: OutputHandler };
+
+export const CORE_DIR = "core", SRC_DIR = "src";
+
+export enum InterpretationStatus {
+  OK = 0,
+  COMPILATION_ERROR = 1,
+  RUNTIME_ERROR = 2,
+}
+
+class Engine {
+  runtime?: NatModule;
+  stdOutHandlers: OutputHandlerMap;
+  stdErrHandlers: OutputHandlerMap;
+
+  constructor() {
+    this.runtime = undefined;
+    this.stdOutHandlers = { console: console.log };
+    this.stdErrHandlers = { console: console.error };
+  }
+
+  print = (stdout: string) => {
+    process.stdout.write(`calling print handlers\n`);
+    Object.values(this.stdOutHandlers).forEach(handler => handler(stdout));
+  }
+  printErr = (stderr: string) => Object.values(this.stdOutHandlers).forEach(handler => handler(stderr));
+
+  onStdout = (handler: OutputHandler) => {
+    let uid = v4();
+
+    this.stdOutHandlers[uid] = handler;
+
+    process.stdout.write(`adding handler: ${uid}\n`);
+
+    return () => {
+      delete this.stdOutHandlers[uid];
+    };
+  }
+
+  loadRuntime = async (): Promise<NatModule> => {
+    if (!this.runtime) this.runtime = await initialize({
+      print: this.print.bind(this),
+      printErr: this.printErr.bind(this)
+    });
+    return this.runtime as NatModule;
+  }
+
+  compile = async (path: string, source: string) => {
+    const runtime = await this.loadRuntime();
+    const strings = [path, source];
+    const fn = runtime.cwrap('vmInterpretEntrypoint_wasm', 'number', ['string', 'number', 'number']);
+
+    let out = "";
+    const unregister = this.onStdout(stdout => {
+      process.stdout.write(`on stdout: ${stdout}`);
+      out += stdout;
+    });
+
+    // allocate memory for the array of pointers.
+    const ptrArray = runtime._malloc(strings.length * 4); // 4 bytes for each pointer.
+
+    // populate the array of pointers.
+    for (let i = 0; i < strings.length; ++i) {
+      const length = strings[i].length + 1;
+      const strPtr = runtime._malloc(length); // +1 for null terminator.
+      runtime.stringToUTF8(strings[i], strPtr, length);
+      runtime.setValue(ptrArray + i * 4, strPtr, '*');
+    }
+
+    const status = fn("src/core/nls", strings.length, ptrArray);
+
+    // free the allocated memory.
+    for (let i = 0; i < strings.length; ++i) {
+      runtime._free(runtime.getValue(ptrArray + i * 4, '*'));
+    }
+    runtime._free(ptrArray);
+
+    // release the stdout handler.
+    unregister();
+    return status === InterpretationStatus.OK ? JSON.parse(out) as Compilation : undefined;
+  }
+
+  interpret = async (path: string): Promise<InterpretationStatus> => {
+    const runtime = await this.loadRuntime();
+    const fn = runtime.cwrap('vmInterpretModule_wasm', 'number', ['string']);
+    return fn(path);
+  };
+
+  getCoreFiles = async (dir = CORE_DIR) => {
+    const runtime = await this.loadRuntime();
+    const files: CoreFile[] = [{
+      path: dir,
+      type: "tree",
+      content: ""
+    }];
+
+    const abs = (path: string) => `/${SRC_DIR}/${path}`;
+
+    runtime.FS.readdir(abs(dir)).forEach(async file => {
+      if ([".", ".."].includes(file)) return;
+
+      let path = `${dir}/${file}`;
+      let stat = runtime.FS.stat(abs(path));
+
+      if (runtime.FS.isDir(stat.mode)) {
+        files.push(...await this.getCoreFiles(path));
+      } else {
+        let content = runtime.FS.readFile(abs(path), { encoding: "utf8" });
+        files.push({ path, type: "blob", content });
+      }
+    });
+
+    return files;
+  };
+
+  mkDir = async (path: string): Promise<void> => {
+    const runtime = await this.loadRuntime();
+    const absPath = `/${SRC_DIR}/${path}`;
+
+    runtime.FS.mkdir(absPath);
+  }
+
+  getFile = async (path: string): Promise<CoreFile> => {
+    const runtime = await this.loadRuntime();
+    const absPath = `/${SRC_DIR}/${path}`;
+    const content = runtime.FS.readFile(absPath, { encoding: "utf8" });
+
+    return { path, content, type: "blob" };
+  }
+
+  setFile = async (path: string, content: string) => {
+    const runtime = await this.loadRuntime();
+    const absPath = `/${SRC_DIR}/${path}`;
+
+    runtime.FS.writeFile(absPath, content, { flags: "w+" });
+  }
+}
+
+export default Engine;
--- wasm/test.ts
+++ wasm/test.ts
@@ -1,11 +1,34 @@
-import init, { NatModule } from './lib/nat.js';
 
-let test = (path: string) => init().then(
-  (mod: NatModule) => {
-    mod.ccall("vmInterpretModule_wasm", "number", ["string"], [path])
-  }
-);
+import Engine, { InterpretationStatus } from './src';
 
-test("test/integration/index");
-test("test/regression/index");
-test("test/trip/index");
+let checkStatus = (status: InterpretationStatus) => {
+  if (status !== InterpretationStatus.OK)
+    throw new Error(`Expected status: ${InterpretationStatus.OK}`);
+}
+
+(async () => {
+  let engine = new Engine();
+  let status: InterpretationStatus;
+
+  status = await engine.interpret("test/integration/index");
+  checkStatus(status);
+
+  status = await engine.interpret("test/regression/index");
+  checkStatus(status);
+
+  status = await engine.interpret("test/trip/index");
+  checkStatus(status);
+
+  let compilation = await engine.compile("foo", "let z = [1 3 4];");
+
+  if (!compilation)
+    throw new Error("Complation failure: undefined.");
+  if (!compilation.success)
+    throw new Error("Complation failure: expecting success=true.");
+  if (!(compilation.data instanceof Array))
+    throw new Error("Complation failure: expecting typeof data=Array.");
+
+})();
+
+
+
