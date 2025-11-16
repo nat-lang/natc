55ec814307c3b361db0574c4059a6973b7fddef4
Alex Shilen <ashilen1@jhu.edu>
2025-11-16 11:37:46 -0500

    refactor: refine switch parsing

--- src/test.c
+++ src/test.c
@@ -3150,7 +3150,7 @@ static AstNode* wrapComprehensionInClosure(AstNode* comp) {
   return newCallNode(builder);
 }
 
-bool testSequenceComprehension() {
+bool testSequenceComprehensionSimple() {
   AstNode* actual = compile("(x | x in (1,2), x != 2)");
 
   AstNode* expected = mkFunction();
@@ -3184,7 +3184,7 @@ bool testSequenceComprehension() {
   return assertNodesEqual(actual, expected);
 }
 
-bool testSetComprehensionParse() {
+bool testSetComprehensionSimple() {
   AstNode* actual = compile("({x | x in (1,2), x != 2})");
 
   AstNode* expected = mkFunction();
@@ -4743,14 +4743,15 @@ int testMain(void) {
   fmt("    ", testPropertySet(), "Property set");
   fmt("    ", testPropertyNested(), "Property nested");
   fmt("    ", testPropertyNestedAssignment(), "Property nested assignment");
-  fmt("    ", testSequenceComprehension(), "Sequence comprehension");
+  fmt("    ", testSequenceComprehensionSimple(),
+      "Sequence comprehension simple");
   fmt("    ", testSequenceComprehensionComplexBody(),
       "Sequence comprehension complex body");
   fmt("    ", testSequenceComprehensionNestedBody(),
       "Sequence comprehension nested body");
   fmt("    ", testSequenceComprehensionNestedCondition(),
       "Sequence comprehension nested condition");
-  fmt("    ", testSetComprehensionParse(), "Set comprehension parse");
+  fmt("    ", testSetComprehensionSimple(), "Set comprehension parse simple");
   fmt("    ", testSetComprehensionComplexBody(),
       "Set comprehension complex body");
   fmt("    ", testSetComprehensionNestedBody(),
